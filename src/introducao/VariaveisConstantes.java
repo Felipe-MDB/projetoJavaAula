5434074cafced460759fd1648ad4ddc6645c6dba
Felipe-MDB <femarques.brito@outlook.com>
2026-04-16 16:59:53 -0300

    Inicializando meu projeto

---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,23 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		// Variaveis
+		int idade = 34;
+		String nome = "Felipe";
+		float graus = 20.0f;	// float => ponto flutuante
+		boolean resposta;	// boolean => logico true/false
+		
+		// Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.print(nome); // print não pula linha
+		System.out.println(QUANTIDADE); // println pula a linha
+		System.out.println(graus);
+	}
+
+}
