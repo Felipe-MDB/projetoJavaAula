8e5346fe23ec0beea34eb4cc2b16d7398cbaaeeb
Felipe-MDB <femarques.brito@outlook.com>
2026-04-22 10:25:51 -0300

    Codigos sobre estruturas condicionais

---
+++ src/lacoCondicional/ExemploIf.java
@@ -0,0 +1,19 @@
+package lacoCondicional;
+
+import java.util.Scanner;
+
+public class ExemploIf {
+
+	public static void main(String[] args) {
+		
+       Scanner leia = new Scanner(System.in); 
+       int idade; // 0,1,2,3...
+       
+       System.out.println("Digite sua idade: ");
+       idade = leia.nextInt();
+       
+       System.out.println("Você pode dirigir");
+   
+	}
+
+}
